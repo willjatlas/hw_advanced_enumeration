5ccc6b3b685df00bca0879faa2c73a86e294081a
William Anderson <willjatlas@googlemail.com>
2020-12-09 20:54:27 +0000

    Pangram first test passed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
-PangramFinder.prototype.isPangram = function () {
 
+PangramFinder.prototype.isPangram = function () {
+  const lowerPhrase = this.phrase.toLowerCase();
+  console.log(lowerPhrase);
+  return this.alphabet.every(element => lowerPhrase.includes(element));
 }
 
 module.exports = PangramFinder;
--- pangram_finder/pangram_finder_spec.js
+++ pangram_finder/pangram_finder_spec.js
@@ -7,32 +7,32 @@ describe('PangramFinder', function () {
     assert.strictEqual(pangramFinder.isPangram(), true);
   });
 
-  it('should be able to detect a non-pangram', function () {
+  xit('should be able to detect a non-pangram', function () {
     const pangramFinder = new PangramFinder('this is not a pangram so the result should be false');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
 
-  it('should be able to detect a pangram with mixed case', function () {
+  xit('should be able to detect a pangram with mixed case', function () {
     const pangramFinder = new PangramFinder('The FIVE boxinG WiZaRdS JUMP quickly');
     assert.strictEqual(pangramFinder.isPangram(), true);
   });
 
-  it('should be able to detect a non-pangram with mixed case', function () {
+  xit('should be able to detect a non-pangram with mixed case', function () {
     const pangramFinder = new PangramFinder('AGaIn ThIs Is NOt a PanGram as It DOeS NoT COnTaIn aLL the LetTerS oF THE ALPhaBet');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
 
-  it('should be able to detect a pangram with special characters', function () {
+  xit('should be able to detect a pangram with special characters', function () {
     const pangramFinder = new PangramFinder('how_vexingly_quick_daft_zebras_jump!');
     assert.strictEqual(pangramFinder.isPangram(), true);
   });
 
-  it('should be able to detect a non-pangram with special characters', function () {
+  xit('should be able to detect a non-pangram with special characters', function () {
     const pangramFinder = new PangramFinder('look_just_clear_this_up:_is_this_a_pangram_or_not?!');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
 
-  it('should be able to handle an empty string', function () {
+  xit('should be able to handle an empty string', function () {
     const pangramFinder = new PangramFinder('');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });

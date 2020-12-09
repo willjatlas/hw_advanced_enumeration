71e73eb608e2b4e858a96217f48d976aebbf7c56
William Anderson <willjatlas@googlemail.com>
2020-12-09 21:36:42 +0000

    isogram first test passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,12 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const wordLetterArray = this.word.split('');
+    return wordLetterArray.every((element, letter) => 
+        this.word.indexOf(element) == letter);
 }
 
 module.exports = IsogramFinder;
+
--- isogram_finder/isogram_finder_spec.js
+++ isogram_finder/isogram_finder_spec.js
@@ -7,17 +7,17 @@ describe('IsogramFinder', function () {
     assert.strictEqual(isogramFinder.isIsogram(), true);
   });
 
-  it('should be able to detect a non-isogram', function () {
+  xit('should be able to detect a non-isogram', function () {
     const isogramFinder = new IsogramFinder('repeated');
     assert.strictEqual(isogramFinder.isIsogram(), false);
   });
 
-  it('should be able to detect an isogram case insensitively', function () {
+  xit('should be able to detect an isogram case insensitively', function () {
     const isogramFinder = new IsogramFinder('Uncopyrightable');
     assert.strictEqual(isogramFinder.isIsogram(), true);
   });
 
-  it('should be able to detect a non-isogram case insensitively', function () {
+  xit('should be able to detect a non-isogram case insensitively', function () {
     const isogramFinder = new IsogramFinder('NotAnIsogram');
     assert.strictEqual(isogramFinder.isIsogram(), false);
   });

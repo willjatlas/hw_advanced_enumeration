1f5c8d1102f46da0884fee640c7fd5e460dbdd50
William Anderson <willjatlas@googlemail.com>
2020-12-09 21:55:51 +0000

    mvp done

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,10 @@
 const AnagramFinder = function (word) {
-
+    this.word = word.toLowerCase();
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    const wordLetterList = this.word.split('');
+    
 }
 
 module.exports = AnagramFinder;
